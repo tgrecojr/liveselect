d1eae9729099ccb4f2eaf50310fedf09ac6bb7a8
T.J. Greco <tgrecojr@gmail.com>
2026-05-12 07:09:32 -0400

    initial checkin

---
+++ LiveSelect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveSelect
+//
+//  Created by T.J. Greco on 5/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveSelect/LiveSelectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveSelectApp.swift
+//  LiveSelect
+//
+//  Created by T.J. Greco on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct LiveSelectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
